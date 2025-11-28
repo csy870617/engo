ff82f619930698079d7a85187d05a392d9c38e22
csy870617 <csy0645009@gmail.com>
2025-11-28 09:59:33 -0800

    Update service-worker.js
    Signed-off-by: csy870617 <csy0645009@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'engo-cache-v37';
+const CACHE_NAME = 'engo-cache-v38';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -43,6 +43,7 @@ self.addEventListener('activate', (event) => {
   );
 
 });
+
 
 
 
