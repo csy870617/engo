de64e73ea407ac8d653ccbd8f6920cb8ba09bc87
csy870617 <csy0645009@gmail.com>
2025-12-07 16:11:08 -0800

    Update service-worker.js
    Signed-off-by: csy870617 <csy0645009@gmail.com>

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // 버전을 v5로 올렸습니다. (수정 시마다 숫자 올리기)
-const CACHE_NAME = 'engo-cache-v81';
+const CACHE_NAME = 'engo-cache-v82';
 
 // 캐싱할 파일 목록
 const ASSETS_TO_CACHE = [
@@ -61,3 +61,4 @@ self.addEventListener('fetch', (event) => {
   );
 
 });
+
